22c1ff248deac4f2b88ccee1c34518b2ace1a7b4
Michael Sarkisian <msarkisian1996@gmail.com>
2022-11-23 21:38:26 -0500

    random word functionality

--- src/words.rs
+++ src/words.rs
@@ -1,12 +1,13 @@
+use rand::{seq::SliceRandom, thread_rng};
 use std::fs;
 
-fn read_words() -> Result<Vec<String>, std::io::Error> {
+pub fn read_words() -> Result<Vec<String>, std::io::Error> {
     let text = fs::read_to_string("./words.txt")?;
     let vec: Vec<String> = text.lines().map(|w| w.to_string()).collect();
     Ok(vec)
 }
 
-fn find_n_length_words(words: &Vec<String>, n: usize) -> Vec<String> {
+pub fn get_all_n_length_words(words: &Vec<String>, n: usize) -> Vec<String> {
     let mut output = Vec::new();
 
     for word in words.iter() {
@@ -17,6 +18,10 @@ fn find_n_length_words(words: &Vec<String>, n: usize) -> Vec<String> {
     output
 }
 
+pub fn get_random_word(words: &Vec<String>) -> Option<&String> {
+    words.choose(&mut thread_rng())
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -25,13 +30,22 @@ mod tests {
     fn find_lengthed_words() {
         let words: Vec<String> = read_words().unwrap();
 
-        let twelve_length_words = find_n_length_words(&words, 12);
-        for word in twelve_length_words.iter() {
-            println!("{}", word)
+        let twelve_length_words = get_all_n_length_words(&words, 12);
+        for _word in twelve_length_words.iter() {
+            // println!("{}", word)
         }
-        let fifteen_length_words = find_n_length_words(&words, 15);
-        for word in fifteen_length_words.iter() {
-            println!("{}", word)
+        let fifteen_length_words = get_all_n_length_words(&words, 15);
+        for _word in fifteen_length_words.iter() {
+            // println!("{}", word)
         }
+    }
+
+    #[test]
+    fn get_random_n_length_word() {
+        let words: Vec<String> = read_words().unwrap();
+        let twelve_length_words = get_all_n_length_words(&words, 12);
+        let random_twelve_length_word = get_random_word(&twelve_length_words);
+
+        assert_eq!(random_twelve_length_word.unwrap().chars().count(), 12);
     }
 }
