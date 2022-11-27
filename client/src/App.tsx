88271e4ba2d981bc715555b8822bd208486f35bb
Michael Sarkisian <msarkisian1996@gmail.com>
2022-11-27 10:11:25 -0500

    dummy data for frontend dev

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+  const dummyGrid = [
+    ['f', 'e', 'b', 'r', 'n'],
+    ['n', 'o', 'e', 'b', 'h'],
+    ['n', 'v', 'r', 'e', 'g'],
+    ['t', 'e', 'm', 'c', 'b'],
+    ['r', 'f', 'y', 'l', 'v'],
+  ];
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div
+      id="gameGrid"
+      style={{
+        display: 'grid',
+        gridTemplateColumns: '1fr '.repeat(dummyGrid.length),
+      }}
+    >
+      {dummyGrid.map((row) => row.map((column) => <div>{column}</div>))}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
