eed3b9431cb89e3105e82c9aeb9d22e722f2555b
Michael Sarkisian <msarkisian1996@gmail.com>
2022-11-27 17:08:04 -0500

    gameboard state and dummy functions

--- client/src/components/gameboard/GameBoard.tsx
+++ client/src/components/gameboard/GameBoard.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Letter } from './Letter';
 import './GameBoard.css';
+import { GameGrid } from '../../@types';
 
 interface GameBoardProps {}
 
 export const GameBoard: React.FC<GameBoardProps> = ({}) => {
+  const [grid, setGrid] = useState<GameGrid | null>(null);
+  const [foundWords, setFoundWords] = useState<string[]>([]);
+  const [score, setScore] = useState(0);
+  // not setting this to falses on initialization to account for different game sizes
+  const [selectedLetters, setSelectedLetters] = useState<boolean[][] | null>(
+    null
+  );
+  const [selectedWord, setSelectedWord] = useState('');
+
+  const handleMouseDown = (y: number, x: number) => {
+    console.log(y, x);
+  };
+
   const dummyGrid = [
     ['f', 'e', 'b', 'r', 'n'],
     ['n', 'o', 'e', 'b', 'h'],
@@ -12,14 +26,36 @@ export const GameBoard: React.FC<GameBoardProps> = ({}) => {
     ['t', 'e', 'm', 'c', 'b'],
     ['r', 'f', 'y', 'l', 'v'],
   ];
+
+  if (!grid) {
+    return (
+      <button
+        onClick={() => {
+          setGrid(dummyGrid);
+          setSelectedLetters(Array(5).fill(Array(5).fill(false)));
+        }}
+      >
+        load dummy grid
+      </button>
+    );
+  }
+
   return (
     <div
       className="gameGrid"
       style={{
-        gridTemplateColumns: '1fr '.repeat(dummyGrid.length),
+        gridTemplateColumns: '100px '.repeat(dummyGrid.length),
       }}
     >
-      {dummyGrid.map((row) => row.map((column) => <Letter letter={column} />))}
+      {grid.map((row, y) =>
+        row.map((column, x) => (
+          <Letter
+            letter={column}
+            selected={selectedLetters![y][x]}
+            handleMouseDown={() => handleMouseDown(y, x)}
+          />
+        ))
+      )}
     </div>
   );
 };
--- client/src/components/gameboard/Letter.tsx
+++ client/src/components/gameboard/Letter.tsx
@@ -2,9 +2,20 @@ import React from 'react';
 
 interface LetterProps {
   letter: string;
+  selected: boolean;
+  handleMouseDown: React.MouseEventHandler;
+
   // onHover, onClick, etc
 }
 
-export const Letter: React.FC<LetterProps> = ({ letter }) => {
-  return <div>{letter}</div>;
+export const Letter: React.FC<LetterProps> = ({
+  letter,
+  selected,
+  handleMouseDown,
+}) => {
+  return (
+    <div className="gameLetter" onMouseDown={handleMouseDown}>
+      {letter.toUpperCase()}
+    </div>
+  );
 };
