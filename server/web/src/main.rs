0e561fc0d4b51c482f07585630f3bf14f1c6f7f8
Michael Sarkisian <msarkisian1996@gmail.com>
2022-12-06 18:14:25 -0500

    static file hosting for built app

--- server/web/src/main.rs
+++ server/web/src/main.rs
@@ -1,12 +1,13 @@
 mod game;
 
 use axum::{routing::get, Router};
+use axum_extra::routing::SpaRouter;
 use game::get_new_game;
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-        .route("/", get(|| async { "Hello World!" }))
+        .merge(SpaRouter::new("/assets", "../client/dist/assets").index_file("../index.html"))
         .route("/game/:size", get(get_new_game));
 
     axum::Server::bind(&"127.0.0.1:8080".parse().unwrap())
