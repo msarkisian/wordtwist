75f540b8254920f328f88ce33a856401b6c57078
Michael Sarkisian <msarkisian1996@gmail.com>
2022-12-09 09:40:02 -0500

    add route for fetching previous games by id

--- server/web/src/game.rs
+++ server/web/src/game.rs
@@ -1,8 +1,9 @@
 use axum::{extract::Path, http::StatusCode, response::IntoResponse, Json};
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 use wordtwist::game::GeneratedGame;
 
-use crate::db::open_db_connection;
+use crate::db::{game::get_game_by_id, open_db_connection};
 
 #[derive(Debug, Deserialize, Serialize, PartialEq, Eq)]
 pub struct Game {
@@ -31,4 +32,26 @@ pub async fn get_new_game(Path(size): Path<usize>) -> impl IntoResponse {
         ));
     }
     Ok((StatusCode::OK, Json(Game::new(size))))
+}
+
+pub async fn get_existing_game_by_id(Path(id): Path<String>) -> impl IntoResponse {
+    let conn = &mut open_db_connection();
+    let id = match Uuid::parse_str(&id) {
+        Ok(id) => id,
+        Err(_) => return Err((StatusCode::BAD_REQUEST, "Cannot parse provided id")),
+    };
+    let game = match get_game_by_id(conn, id) {
+        Ok(game) => game,
+        Err(rusqlite::Error::QueryReturnedNoRows) => {
+            return Err((StatusCode::NOT_FOUND, "Game with specified ID not found"))
+        }
+        Err(_) => {
+            return Err((
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Error fetching game from database",
+            ))
+        }
+    };
+
+    Ok((StatusCode::OK, Json(game)))
 }
--- server/web/src/main.rs
+++ server/web/src/main.rs
@@ -4,15 +4,16 @@ mod game;
 use axum::{routing::get, Router};
 use axum_extra::routing::SpaRouter;
 use db::open_db_connection;
-use game::get_new_game;
+use game::{get_existing_game_by_id, get_new_game};
 
 #[tokio::main]
 async fn main() {
-    let conn = open_db_connection();
+    let _ = open_db_connection();
 
     let app = Router::new()
         .merge(SpaRouter::new("/assets", "../client/dist/assets").index_file("../index.html"))
-        .route("/game/:size", get(get_new_game));
+        .route("/game/:size", get(get_new_game))
+        .route("/game/id/:id", get(get_existing_game_by_id));
 
     axum::Server::bind(&"127.0.0.1:8080".parse().unwrap())
         .serve(app.into_make_service())
