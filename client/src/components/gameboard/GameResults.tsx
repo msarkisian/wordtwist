81b621f2e69df5ad8845c0c60a17e6cf28b4180c
Michael Sarkisian <msarkisian1996@gmail.com>
2023-01-23 15:58:18 -0500

    game results tailwind css

--- client/src/components/gameboard/GameResults.tsx
+++ client/src/components/gameboard/GameResults.tsx
@@ -17,12 +17,17 @@ export const GameResults: React.FC<GameResultsProps> = ({
 }) => {
   const [copiedId, setCopiedId] = useState(false);
   return (
-    <div className="gameResults">
-      <p>
+    <div className="m-2">
+      <h2 className="text-lg mb-2">
         You scored <strong>{score}</strong> points!
-      </p>
-      <button onClick={reset}>Start new game</button>
+      </h2>
+      <button className="btn-primary w-36 h-8 mr-2" onClick={reset}>
+        Start new game
+      </button>
       <button
+        className={`btn-secondary w-28 h-8
+        ${copiedId ? 'cursor-default' : 'cursor-pointer'}
+        `}
         disabled={copiedId}
         onClick={() => {
           navigator.clipboard.writeText(gameId);
@@ -31,15 +36,15 @@ export const GameResults: React.FC<GameResultsProps> = ({
       >
         {copiedId ? <>ID copied!</> : <>Copy Game ID</>}
       </button>
-      <p>Words in this puzzle:</p>
-      <ul>
+      <p className="mt-4">Words in this puzzle:</p>
+      <ul className="list-disc mx-6">
         {foundWords.map((word) => (
-          <li className="foundWord" key={word}>
+          <li className="text-green-800 " key={word}>
             {word}
           </li>
         ))}
         {validWords.map((word) => (
-          <li className="missedWord" key={word}>
+          <li className="text-red-800" key={word}>
             {word}
           </li>
         ))}
