1b2760f9209bd98d25cb5295c92d923153dfe39a
Michael Sarkisian <msarkisian1996@gmail.com>
2023-02-01 18:08:03 -0500

    search from project root for database file too

--- server/web/src/db.rs
+++ server/web/src/db.rs
@@ -3,5 +3,7 @@ use rusqlite::Connection;
 pub mod game;
 
 pub fn open_db_connection() -> Connection {
-    Connection::open("./web/sql.db3").expect("error connecting to database")
+    Connection::open("./server/web/sql.db3").unwrap_or_else(|_| {
+        Connection::open("./web/sql.db3").expect("error connecting to database")
+    })
 }
