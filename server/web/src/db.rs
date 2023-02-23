bc6e8f278495b3b8a57a643b4d55793387beb609
Michael Sarkisian <msarkisian1996@gmail.com>
2023-02-23 10:25:31 -0500

    conditional db path for test compilations

--- server/web/src/db.rs
+++ server/web/src/db.rs
@@ -2,8 +2,14 @@ use rusqlite::Connection;
 
 pub mod game;
 
+#[cfg(test)]
+const DB_PATH: &'static str = "./sql.db3";
+#[cfg(not(test))]
+const DB_PATH: &'static str = ".server/web/sql.db3";
+const FALLBACK_DB_PATH: &'static str = "./web/sql.db3";
+
 pub fn open_db_connection() -> Connection {
-    Connection::open("./server/web/sql.db3").unwrap_or_else(|_| {
-        Connection::open("./web/sql.db3").expect("error connecting to database")
+    Connection::open(DB_PATH).unwrap_or_else(|_| {
+        Connection::open(FALLBACK_DB_PATH).expect("error connecting to database")
     })
 }
