cd09235be96bb64b60b2ba4ce88a514a89559524
Michael Sarkisian <msarkisian1996@gmail.com>
2023-02-23 11:04:09 -0500

    remove redundant constant static lifetimes

--- server/web/src/db.rs
+++ server/web/src/db.rs
@@ -3,10 +3,10 @@ use rusqlite::Connection;
 pub mod game;
 
 #[cfg(test)]
-const DB_PATH: &'static str = "./sql.db3";
+const DB_PATH: &str = "./sql.db3";
 #[cfg(not(test))]
-const DB_PATH: &'static str = ".server/web/sql.db3";
-const FALLBACK_DB_PATH: &'static str = "./web/sql.db3";
+const DB_PATH: &str = ".server/web/sql.db3";
+const FALLBACK_DB_PATH: &str = "./web/sql.db3";
 
 pub fn open_db_connection() -> Connection {
     Connection::open(DB_PATH).unwrap_or_else(|_| {
