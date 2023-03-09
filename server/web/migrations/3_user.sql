fee1c1e46d709c09a514baafb530513f013999d8
Michael Sarkisian <msarkisian1996@gmail.com>
2023-03-09 16:03:08 -0500

    index email and username in database

--- server/web/migrations/3_user.sql
+++ server/web/migrations/3_user.sql
@@ -1,6 +1,9 @@
-CREATE TABLE user (
+CREATE TABLE users (
   id INTEGER PRIMARY KEY,
   email TEXT UNIQUE,
   username TEXT UNIQUE,
   password_hash TEXT
 )
+
+CREATE INDEX idx_users_email ON users (email);
+CREATE INDEX idx_users_username ON users (username);
