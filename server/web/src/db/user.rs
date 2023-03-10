9ee52f33ef50afa3fb61a2f442d46ab362573d67
Michael Sarkisian <msarkisian1996@gmail.com>
2023-03-09 19:23:08 -0500

    map argon2 error to anyhow error message

--- server/web/src/db/user.rs
+++ server/web/src/db/user.rs
@@ -1,6 +1,6 @@
 use std::{error::Error, fmt::Display};
 
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use argon2::{
     password_hash::{rand_core::OsRng, SaltString},
     Argon2, PasswordHasher,
@@ -8,17 +8,6 @@ use argon2::{
 use rusqlite::Connection;
 
 pub struct UserID(pub usize);
-// Couldn't get argon2's errors to play nice with anyhow
-// may refactor this custom error type later
-#[derive(Debug)]
-pub struct HashError;
-
-impl Display for HashError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "error hashing password")
-    }
-}
-impl Error for HashError {}
 
 pub fn add_user(
     conn: &mut Connection,
@@ -31,7 +20,7 @@ pub fn add_user(
 
     let password_hash = argon2
         .hash_password(password.as_bytes(), &salt)
-        .map_err(|_| HashError)?;
+        .map_err(|e| anyhow!("error hashing password: {:?}", e))?;
 
     let user_id = conn.query_row(
         "INSERT INTO users (email, username, password_hash) VALUES (?1, ?2, ?3) RETURNING id",
