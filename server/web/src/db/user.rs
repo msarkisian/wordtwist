104cdab1be411b6893de3141e84a38be034e4843
Michael Sarkisian <msarkisian1996@gmail.com>
2023-03-12 11:00:13 -0400

    `validate_user` db func

--- server/web/src/db/user.rs
+++ server/web/src/db/user.rs
@@ -1,9 +1,7 @@
-use std::{error::Error, fmt::Display};
-
 use anyhow::{anyhow, Result};
 use argon2::{
     password_hash::{rand_core::OsRng, SaltString},
-    Argon2, PasswordHasher,
+    Argon2, PasswordHash, PasswordHasher, PasswordVerifier,
 };
 use rusqlite::Connection;
 
@@ -28,4 +26,24 @@ pub fn add_user(
         |r| Ok(UserID(r.get(0)?)),
     )?;
     Ok(user_id)
+}
+
+pub fn validate_user(conn: &mut Connection, username: &str, password: &str) -> Result<UserID> {
+    let (id, password_hash) = conn.query_row(
+        "SELECT id, password_hash FROM users WHERE username=?1",
+        (username,),
+        |r| Ok((r.get::<usize, usize>(0), r.get::<usize, String>(1))),
+    )?;
+    let password_hash = password_hash.unwrap_or_default();
+    let parsed_hash =
+        PasswordHash::new(&password_hash).map_err(|_| anyhow!("invalid username or password"))?;
+
+    if Argon2::default()
+        .verify_password(password.as_bytes(), &parsed_hash)
+        .is_err()
+    {
+        return Err(anyhow!("invalid username or password"));
+    }
+
+    Ok(UserID(id.unwrap()))
 }
