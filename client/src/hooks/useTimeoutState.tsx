e105f5db0b19cd25c36b9ba5552d7989831d28e9
Michael Sarkisian <msarkisian1996@gmail.com>
2023-03-16 14:42:21 -0400

    clear previous timeouts on new setState

--- client/src/hooks/useTimeoutState.tsx
+++ client/src/hooks/useTimeoutState.tsx
@@ -1,10 +1,16 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 // `useState` equilivant that reverts state back to `null` after timeout expires
 export const useTimeoutState = <T,>(timeout: number) => {
   const [state, setState] = useState<T | null>(null);
+  const timeoutRef = useRef<number | null>(null);
+
   const wrappedSetState = (newState: T) => {
     setState(newState);
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+      timeoutRef.current = null;
+    }
     setTimeout(() => {
       setState(null);
     }, timeout);
