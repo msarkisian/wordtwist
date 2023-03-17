b890c5ae47ea00e819b461c4c67745977ca8c06b
Michael Sarkisian <msarkisian1996@gmail.com>
2023-03-17 09:43:39 -0400

    refactor score funcs to game.rs

--- server/web/src/db.rs
+++ server/web/src/db.rs
@@ -1,7 +1,6 @@
 use rusqlite::Connection;
 
 pub mod game;
-pub mod score;
 pub mod user;
 
 #[cfg(test)]
--- server/web/src/db/game.rs
+++ server/web/src/db/game.rs
@@ -5,6 +5,8 @@ use wordtwist::game::Game as GameData;
 
 use crate::game::Game;
 
+use super::user::UserID;
+
 pub fn get_game_by_id(conn: &mut Connection, id: Uuid) -> Result<GameData> {
     conn.query_row_and_then(
         "SELECT game_data from GAMES where id=?1",
@@ -51,6 +53,27 @@ pub fn set_daily(conn: &mut Connection, id: Uuid) -> Result<()> {
     Ok(())
 }
 
+pub fn add_game_score(
+    conn: &mut Connection,
+    game_id: Uuid,
+    user_id: UserID,
+    score: usize,
+) -> Result<()> {
+    conn.execute(
+        "INSERT INTO scores (game_id, user_id, score) VALUES (?1, ?2, ?3)",
+        (game_id.to_string(), user_id.0, score),
+    )?;
+    Ok(())
+}
+
+pub fn get_game_score(conn: &mut Connection, game_id: Uuid, user_id: UserID) -> Result<usize> {
+    conn.query_row(
+        "SELECT score FROM scores WHERE game_id=?1 AND user_id=?2",
+        (game_id.to_string(), user_id.0),
+        |r| r.get(0),
+    )
+}
+
 #[cfg(test)]
 mod test {
     use crate::db::open_db_connection;
@@ -66,5 +89,26 @@ mod test {
         let fetched_game = get_game_by_id(&mut connection, game_id).unwrap();
 
         assert_eq!(game, fetched_game);
+    }
+
+    #[test]
+    fn test_set_get_score() {
+        let mut conn = Connection::open_in_memory().unwrap();
+        conn.execute(
+            "CREATE TABLE scores (
+                id INTEGER PRIMARY KEY,
+                game_id TEXT,
+                user_id INTEGER,
+                score, INTEGER
+            );",
+            (),
+        )
+        .unwrap();
+
+        let uid = UserID(500);
+        let game_uuid = Uuid::new_v4();
+
+        add_game_score(&mut conn, game_uuid, uid, 9001).unwrap();
+        assert_eq!(get_game_score(&mut conn, game_uuid, uid).unwrap(), 9001)
     }
 }
