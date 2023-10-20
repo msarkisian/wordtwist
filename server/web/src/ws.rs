77278827618f2cc15b04e00d7d235c472e6fdf31
Michael Sarkisian <msarkisian1996@gmail.com>
2023-10-19 22:14:09 -0400

    simplify socket logic, remove oneshot channel

--- server/web/src/ws.rs
+++ server/web/src/ws.rs
@@ -3,7 +3,6 @@ use std::{net::SocketAddr, time::Duration};
 use axum::extract::ws::{Message, WebSocket};
 use serde::Serialize;
 use serde_json;
-use tokio::sync::oneshot;
 use wordtwist::game::GameResults;
 
 use crate::game::Game;
@@ -27,52 +26,38 @@ pub async fn handle_socket_game(mut socket: WebSocket, _: SocketAddr, game: Game
         .await
         .is_ok();
 
-    let mut timeout = tokio::spawn(async {
-        tokio::time::sleep(Duration::from_secs(GAME_TIME)).await;
-    });
-    let (tx_done, mut rx_done) = oneshot::channel::<()>();
+    let mut timeout = Box::pin(async { tokio::time::sleep(Duration::from_secs(GAME_TIME)).await });
 
-    let mut process_words = tokio::spawn(async move {
+    tokio::spawn(async move {
         let mut submitted_words = Vec::with_capacity(game.data.valid_words().len());
 
         loop {
             tokio::select! {
-                _ = &mut rx_done => {
+                _ = &mut timeout => {
                     let _ = socket.send(Message::Text(serde_json::to_string(&SocketResponse::GameOver { results: game.data.score(submitted_words) }).unwrap())).await.is_err();
                     break;
-                },
+                }
                 s = socket.recv() => {
-                    match s {
-                        Some(Ok(msg)) => {
-                            if let Message::Text(word) = msg {
-                                if game.data.valid_words().contains(&word) && !submitted_words.contains(&word) {
-                                    submitted_words.push(word);
-                                    if socket.send(Message::Text(serde_json::to_string(&SocketResponse::GuessResponse {valid: true, word: submitted_words.last().unwrap()}).unwrap())).await.is_err() {
-                                        break;
-                                    }
-                                // } else if socket.send(Message::Text("false".to_string())).await.is_err() {
-                                } else if socket.send(Message::Text(serde_json::to_string(&SocketResponse::GuessResponse {valid: false, word: &word}).unwrap())).await.is_err() {
-                                    break;
-                                }
-                            }
-                        },
-                        _ => {
-                            break;
+                    if let Some(Ok(Message::Text(word))) = s {
+                        if game.data.valid_words().contains(&word) && !submitted_words.contains(&word) {
+                            submitted_words.push(word);
+                            let _ = socket.send(Message::Text(
+                                serde_json::to_string(&SocketResponse::GuessResponse {
+                                     word: submitted_words.last().unwrap(), valid: true
+                                    }).unwrap()
+                            )).await;
+                        } else {
+                            let _ = socket.send(Message::Text(
+                                serde_json::to_string(&SocketResponse::GuessResponse {
+                                word: &word, valid: false
+                                }).unwrap()
+                            )).await;
                         }
+                    } else {
+                        break;
                     }
                 }
             }
         }
     });
-
-    tokio::select! {
-        _ = (&mut timeout) => {
-            // stop the process word task, signal the client
-            tx_done.send(()).unwrap();
-        }
-        _ = (&mut process_words) => {
-        // if client hangs up, cancel timeout
-        timeout.abort();
-        }
-    };
 }
